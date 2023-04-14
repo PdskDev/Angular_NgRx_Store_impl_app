82a5ba48395b1fa049c6116b6def02dcb48cc86c
PdskDev <prestadesk@gmail.com>
2023-04-14 23:36:17 +0200

    add @ngrx - create 1-Action, 2-Reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/store/2-app.reducer.ts
@@ -0,0 +1,5 @@
+import * as AppActions from './1-app.actions';
+
+import { Action, createReducer, on } from '@ngrx/store';
+
+import { IFruit } from '../interfaces/fruit.interface';
